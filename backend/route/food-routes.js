2f7e54c3f78d5d93a1a2f13c916aa18ee027b28e
Ifeoluwa <ifel1939@gmail.com>
2023-06-14 05:55:37 +0100

    updating route with food route

---
+++ backend/route/food-routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createFood,
+  getAllFoods,
+  getAllFoodsByUser,
+  deleteFood,
+  getSingleFood,
+} = require("../controller/food-controller");
+
+router.route("/").post(createFood).get(getAllFoodsByUser);
+router.route("/:id").get(getSingleFood).delete(deleteFood);
+
+router.get("/", getAllFoods);
+
+module.exports = router;
