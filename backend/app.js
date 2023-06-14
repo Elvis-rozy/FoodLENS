c237b78196be256d9b302c253539188993cb425e
Ifeoluwa <ifel1939@gmail.com>
2023-06-14 06:13:19 +0100

    sign up funtionality and login implemented

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+require("dotenv").config();
+require("express-async-errors");
+const notFound = require("./middleware/notFound");
+const errorHandler = require("./middleware/errorHandler");
+const authRoute = require("./routes/authentication");
+const foodRoute = require("./routes/food-routes");
+const authenticateUser = require("./middleware/authentication");
+const connectdb = require("./db/connectDB");
+const Food = require("./model/Food");
+const foodjason = require("./foodJson");
+
+app.use(express.json());
+
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/foods", authenticateUser, foodRoute);
+
+app.use(errorHandler);
+app.use(notFound);
+
+const start = async () => {
+  try {
+    await connectdb(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`server listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
