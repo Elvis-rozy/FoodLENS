4cf6f65271dca0a694d2928be0ed2924a9d68208
Elvis rozy <elvisrozy52@gmail.com>
2023-06-27 03:54:35 +0100

    new push- seperated js files to avoid conflicts and fixed buttons style among several changes

---
+++ app/js/globals.js
@@ -0,0 +1,39 @@
+/*=====================================
+Mobile menu setup
+=====================================*/
+const Bttn = document.querySelector("#menu");
+const navBar = document.querySelector(".mobile-menu");
+const linK = document.querySelector(".lnk");
+
+/*============================================
+Menu Icon Open and Close Function
+============================================*/
+Bttn.addEventListener("click", () => {
+    //close menu
+    if (Bttn.classList.contains('menu-open')) {
+        console.log('Menu Closed')
+        Bttn.classList.remove('menu-open')
+        navBar.classList.remove('nav-open')
+    }//Open menu
+    else {
+        console.log('Menu Opened')
+        Bttn.classList.add('menu-open')
+        navBar.classList.add('nav-open')
+    }
+})
+//Menu Links Closing Function
+linK.addEventListener('click', () => {
+    Bttn.classList.remove('menu-open')
+    navBar.classList.remove('nav-open')
+})
+
+/*============================================
+Dark mode / Light mode setup
+============================================*/
+const body = document.querySelector("#body");
+const toggleBtn = document.getElementById("toggle-mode");
+
+toggleBtn.addEventListener("click", function() {
+    body.classList.toggle("light-mode");
+    console.log("clicker");
+  });
