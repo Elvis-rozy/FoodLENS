6778c732450c3d017c8fa6aac9fc84a8cfdb07a9
Ifeoluwa <ifel1939@gmail.com>
2023-07-06 21:20:45 +0100

    server rendering the frontend

--- backend/app.js
+++ backend/app.js
@@ -11,13 +11,22 @@ const allFoods=require("./routes/all-foods")
 const authenticateUser = require("./middleware/authentication");
 const connectdb = require("./db/connectDB");
 const cors=require('cors')
-
+const helmet=require("helmet")
+const xss = require("xss-clean");
+const path = require("path");
+app.use(express.static(path.resolve(__dirname, "../frontend")));
 app.use(express.json());
 app.use(cors())
+app.use(helmet());
+app.use(xss());
+
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/foods", authenticateUser, foodRoute);
 app.use("/api/v1/all-foods-item",allFoods)
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend", "index.html"));
+});
 app.use(errorHandler);
 app.use(notFound);
 
