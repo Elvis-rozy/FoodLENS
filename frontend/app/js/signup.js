2c6e1a4d25b7ff8880ffef80c6fc9d59cd12ebf6
Ifeoluwa <ifel1939@gmail.com>
2023-07-07 08:51:58 +0100

    login fixed

--- frontend/app/js/signup.js
+++ frontend/app/js/signup.js
@@ -64,7 +64,7 @@ form.addEventListener("submit", function (e) {
     const data = { username: username, email: email, password: password };
 
     // Make a POST request using fetch API
-    fetch("/api/v1/auth/register", {
+    fetch("https://foodlens.onrender.com/api/v1/auth/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
