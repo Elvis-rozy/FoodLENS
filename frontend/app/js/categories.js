4fb9a11743d39632efafed73a880823ef529f4a6
Elvis rozy <elvisrozy52@gmail.com>
2023-07-11 01:22:52 +0100

    categories page to selected page now functional and new dashboard written

--- frontend/app/js/categories.js
+++ frontend/app/js/categories.js
@@ -1,3 +1,4 @@
+//Get HTML elements
 let buttonDisplay = document.querySelector(".buttons");
 const searchform = document.querySelector(".search-form");
 
@@ -48,13 +49,18 @@ const videoEmbedSRC = document.getElementById('video');
 
 const sectionCenter = document.querySelector(".section-center");
 
+//listening on page load to create categories page elements
 window.addEventListener("DOMContentLoaded", () => {
-  fetch('app/js/fooddata.json').then(response => response.json())
+  //fetch API data containing all food details
+  //fetch('https://backend-hzm1.onrender.com/api/v1/all-foods-item/all')
+  fetch('app/JSON/fooddata.json')
+  .then(response => response.json())
   .then(data => {
-
+    //calling the display functions for the categories page
     displayMenuItem(data);
     displayMenuButton();
 
+    //function that loads the category links
     function displayMenuButton() {
       const categories = data.reduce(
         (newCategory, newItem) => {
@@ -81,28 +87,25 @@ window.addEventListener("DOMContentLoaded", () => {
           const filter = e.target.dataset.filter;
 
           const menuCategory = data.filter(function (menuItem) {
-            //console.log(menuItem.category)
             if (menuItem.category === filter) {
               return menuItem;
             }
           });
-
+          //condition to determine which items are displayed based on selected category
           if (filter == "All") {
             displayMenuItem(data);
-            console.log(data);
-          } else {
-            displayMenuItem(menuCategory);
-            console.log(menuCategory)
+          } else  {
+            displayMenuCategory(menuCategory);
           }
         });
       });
-    }
+    }//catch statement to handle errors
   }).catch(error => console.error(error));
 });
-
+//function that displays All category section
 function displayMenuItem(menuItems) {
   let displayMenu = menuItems.map((item) => {
-    return `<div id "${item.food_name}" class="menu-item">
+    return `<div id="${item.food_name}" class="menu-item">
         <img src=${item.img_url} class="photo" alt="">
         <div class="item-info">
           <h4 id="${item.food_name}">${item.food_name}</h4>
@@ -118,7 +121,8 @@ function displayMenuItem(menuItems) {
   const elements = document.querySelectorAll('.menu-item');
 
   //fetch the json file and convert to javascript object
-  fetch('https://backend-hzm1.onrender.com/api/v1/all-foods-item/all')
+  //fetch('https://backend-hzm1.onrender.com/api/v1/all-foods-item/all')
+  fetch("app/JSON/fooddata.json")
   .then(response => response.json())
   .then(data => {
 
@@ -175,6 +179,91 @@ function displayMenuItem(menuItems) {
 
         videoWatchSRC.textContent = data[`${number}`].video_url;
       })
-    })
+    })//catch statement to handle errors
   }).catch(error => console.error(error));
+}
+
+//function that displays every other category
+function displayMenuCategory(menuItems) {
+  let displayMenu = menuItems.map((item) => {
+    return `<div id="${item.category}" class="menu-item">
+        <img src=${item.img_url} class="photo" alt="">
+        <div class="item-info">
+          <h4 id="${item.food_name}">${item.food_name}</h4>
+          <p class="item-text">${item.cuisine}
+          </p>
+        </div>
+      </div>`;
+  });
+
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+
+  //categories to selection page flow
+  const elements = document.querySelectorAll('.menu-item');
+
+  let arr = ["pastries", "starches", "meat-seafoods", "fruits", "vegetables"];
+
+  // Add click event listeners to each element
+  elements.forEach((element, Index)=>{
+    element.addEventListener('click', ()=>{
+      //hide categories page and show selection page
+      categories.classList.add('Hide');
+      searchform.classList.add('Hide');
+      link.classList.remove('Hide');
+      selection.classList.remove('Hide');
+
+      // Get the unique ID of the clicked element
+      const id = element.getAttribute('id');
+
+      // Calculate the number based on index for dynamic text
+      const number = Index;
+
+      for (i = 0; i < arr.length; i++) {
+
+        if (id === arr[i]) {
+          fetch("app/JSON/" + arr[i] + ".json")
+          .then(response => response.json())
+          .then(data => {
+            // Change the content based on the unique ID
+            myImage.setAttribute("src", data[`${number}`].img_url);
+            foodName.textContent = data[`${number}`].food_name.charAt(0).toUpperCase() + data[`${number}`].food_name.slice(1);
+            foodN.textContent = data[`${number}`].food_name.charAt(0).toUpperCase() + data[`${number}`].food_name.slice(1);;
+            cuisine.textContent = data[`${number}`].cuisine.charAt(0).toUpperCase() + data[`${number}`].cuisine.slice(1);;
+            category.textContent = data[`${number}`].category.charAt(0).toUpperCase() + data[`${number}`].category.slice(1);;
+            description.textContent = data[`${number}`].description.charAt(0).toUpperCase() + data[`${number}`].description.slice(1);;
+            servQty.textContent = data[`${number}`].serving_qty;
+            servUnit.textContent = data[`${number}`].serving_unit.charAt(0).toUpperCase() + data[`${number}`].serving_unit.slice(1);;
+            servWtgram.textContent = data[`${number}`].serving_weight_grams;
+            calories.textContent = data[`${number}`].nutrition_fact.nf_calories;
+            totFats.textContent = data[`${number}`].nutrition_fact.nf_total_fat;
+            satFat.textContent = data[`${number}`].nutrition_fact.nf_saturated_fat;
+            cholesterol.textContent = data[`${number}`].nutrition_fact.nf_cholesterol;
+            sodium.textContent = data[`${number}`].nutrition_fact.nf_sodium;
+            totCarb.textContent = data[`${number}`].nutrition_fact.nf_total_carbohydrate;
+            dietFiber.textContent = data[`${number}`].nutrition_fact.nf_dietary_fiber;
+            sugars.textContent = data[`${number}`].nutrition_fact.nf_sugars;
+            totProtein.textContent = data[`${number}`].nutrition_fact.nf_protein;
+            potassium.textContent = data[`${number}`].nutrition_fact.nf_potassium;
+            iron.textContent = data[`${number}`].nutrition_fact.nf_p;
+            totfatvalue7.textContent = data[`${number}`].nutrition_fact.nf_total_fat;
+            satfatvalue8.textContent = data[`${number}`].nutrition_fact.nf_saturated_fat;
+            cholesvalue10.textContent = data[`${number}`].nutrition_fact.nf_cholesterol;
+            sodiumvalue9.textContent = data[`${number}`].nutrition_fact.nf_sodium;
+            carbvalue1.textContent = data[`${number}`].nutrition_fact.nf_total_carbohydrate;
+            dietfibvalue2.textContent = data[`${number}`].nutrition_fact.nf_dietary_fiber;
+            sugarvalue3.textContent = data[`${number}`].nutrition_fact.nf_sugars;
+            totprotvalue4.textContent = data[`${number}`].nutrition_fact.nf_protein;
+            potassvalue6.textContent = data[`${number}`].nutrition_fact.nf_potassium;
+            ironvalue5.textContent = data[`${number}`].nutrition_fact.nf_p;
+            data[`${number}`].ingredients.forEach(elem=>ingredients.textContent+=(`Ingredient Name: ${elem.ingredient_name}. Serving Quantity: ${elem.serving_qty}. Description: ${elem.description}. Serving Weight grams: ${elem.serving_weight_grams}. Serving Unit: ${elem.serving_unit}. Note: ${elem.note}. Source: ${elem.source}.`));
+            videoEmbedSRC.setAttribute("src", data[`${number}`].video_url);
+            videoWatchSRC.textContent = data[`${number}`].video_url;
+
+          //catch statement to handle errors
+          }).catch(error => console.error(error));
+        }
+      }
+    })
+  })
 }
