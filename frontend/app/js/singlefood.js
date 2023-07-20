74e963dd436a77a3c7d360a8d4e45058596d691c
Ifeoluwa <ifel1939@gmail.com>
2023-07-20 17:38:24 +0100

    category page ready

--- frontend/app/js/singlefood.js
+++ frontend/app/js/singlefood.js
@@ -10,74 +10,71 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 function displayCategoryItem(id) {
-  fetch(`/api/v1/all-foods-item/${id}`).then(
-    (response) => {
-      return response
-        .json()
-        .then((data) => {
-          console.log(data.food);
+  fetch(`/api/v1/all-foods-item/${id}`).then((response) => {
+    return response
+      .json()
+      .then((data) => {
+        console.log(data.food);
+        const {
+          _id,
+          food_name,
+          brand_name,
+          calories,
+          category,
+          allergenInfo,
+          cuisine,
+          description,
+          group,
+          img_url,
+          ingredients,
+          intoleranceInfo,
+          serving_qty,
+          serving_unit,
+          serving_weight_grams,
+          video_url,
+          nutrition_fact,
+        } = data.food;
+
+        const {
+          nf_calories,
+          nf_cholesterol,
+          nf_dietary_fiber,
+          nf_p,
+          nf_potassium,
+          nf_protein,
+          nf_saturated_fat,
+          nf_sodium,
+          nf_sugars,
+          nf_total_carbohydrate,
+          nf_total_fat,
+        } = data.food.nutrition_fact;
+
+        //let nut_fact=JSON.stringify(data.food.nutrition_fact);
+        //console.log(nut_fact)
+
+        let nf_total = 0;
+        for (const key in data.food.nutrition_fact) {
+          if (typeof data.food.nutrition_fact[key] === "number") {
+            nf_total += data.food.nutrition_fact[key];
+            // console.log(nut_fact[key]);
+          }
+        }
+        nf_total = nf_total - nf_calories;
+
+        const ingredientDom = ingredients.map((item, index) => {
           const {
-            _id,
-            food_name,
-            brand_name,
-            calories,
-            category,
-            allergenInfo,
-            cuisine,
-            description,
-            group,
-            img_url,
-            ingredients,
-            intoleranceInfo,
+            ingredient_name,
             serving_qty,
             serving_unit,
             serving_weight_grams,
-            video_url,
-            nutrition_fact,
-          } = data.food;
+          } = item;
 
-          const {
-            nf_calories,
-            nf_cholesterol,
-            nf_dietary_fiber,
-            nf_p,
-            nf_potassium,
-            nf_protein,
-            nf_saturated_fat,
-            nf_sodium,
-            nf_sugars,
-            nf_total_carbohydrate,
-            nf_total_fat,
-          } = data.food.nutrition_fact;
+          return `<p id=${index + 1}>${
+            index + 1
+          }  <span>${ingredient_name}</span><span>${serving_qty} </span> <span>${serving_unit} </span>   </p>`;
+        });
 
-
-//let nut_fact=JSON.stringify(data.food.nutrition_fact);
-//console.log(nut_fact)
-
-          let nf_total = 0;
-          for (const key in data.food.nutrition_fact) {
-            if (typeof data.food.nutrition_fact[key] === "number") {
-              nf_total += data.food.nutrition_fact[key];
-              // console.log(nut_fact[key]);
-              
-            }
-          }
-        nf_total =nf_total-nf_calories;
-
-          const ingredientDom = ingredients.map((item, index) => {
-            const {
-              ingredient_name,
-              serving_qty,
-              serving_unit,
-              serving_weight_grams,
-            } = item;
-
-            return `<p id=${index + 1}>${
-              index + 1
-            }  <span>${ingredient_name}</span><span>${serving_qty} </span> <span>${serving_unit} </span>   </p>`;
-          });
-
-          const shownItem = `<div class="result" id=${_id}>
+        const shownItem = `<div class="result" id=${_id}>
         <img id="img" src=${img_url} alt="food image" />
         <div class="food-details">
           <h3 id="foodName">${food_name}</h3>
@@ -140,41 +137,43 @@ function displayCategoryItem(id) {
 <div class="for-fat">
    <div class="total-fat">
     <div class="t-flex"> <h1>total fat</h1><span><p>(${nf_total_fat}g)</p></span>  </div>
-    <span><p>${parseInt((nf_total_fat / nf_total)*100)}%</p></span>
+    <span><p>${parseInt((nf_total_fat / nf_total) * 100)}%</p></span>
 </div> 
   <div class="total-fat">
     <div class="t-flex"> <h1>saturated fat</h1><span><p>(${nf_saturated_fat}g)</p></span>  </div>
-    <span><p>${parseInt((nf_saturated_fat / nf_total_fat)*100)}%</p></span>
+    <span><p>${parseInt((nf_saturated_fat / nf_total_fat) * 100)}%</p></span>
 </div> 
   <div class="total-fat">
      <div class="t-flex"> <h1>unsaturated</h1><span><p>(${
        nf_total_fat - nf_saturated_fat
      }g)</p></span>  </div>
-    <span>${parseInt(((nf_total_fat - nf_saturated_fat) / nf_total_fat)*100)}%</p></span>
+    <span>${parseInt(
+      ((nf_total_fat - nf_saturated_fat) / nf_total_fat) * 100
+    )}%</p></span>
 </div> 
 </div>
  <div class="total-fat">
     <div class="t-flex"> <h1>sodium</h1><span><p>(${nf_sodium}g)</p></span>  </div>
    
-    <span><p>${parseInt((nf_sodium / nf_total)*100)}%</p></span>
+    <span><p>${parseInt((nf_sodium / nf_total) * 100)}%</p></span>
 </div> 
  <div class="total-fat">
    <div class="t-flex"> <h1>cholesrtrol</h1><span><p>(${nf_cholesterol}g)</p></span>  </div> 
-    <span><p>${parseInt((nf_cholesterol / nf_total)*100)}%</p></span>
+    <span><p>${parseInt((nf_cholesterol / nf_total) * 100)}%</p></span>
 </div> 
  <div class="total-fat">
    <div class="t-flex"> <h1>Dietary Fiber</h1><span><p>(${nf_dietary_fiber}g)</p></span>  </div>
-    <span><p>${parseInt((nf_dietary_fiber / nf_total)*100)}%</p></span>
+    <span><p>${parseInt((nf_dietary_fiber / nf_total) * 100)}%</p></span>
 </div> 
 </main>
 <div class="total-protein">
    <div class="t-flex"> <h1>Protein</h1><span><p>(${nf_protein}g)</p></span>  </div>
-    <span><p>${parseInt((nf_protein / nf_total)*100)}%</p></span>
+    <span><p>${parseInt((nf_protein / nf_total) * 100)}%</p></span>
  </div>
 <div class="border-large"></div>
 <div class="total-fat">
     <div class="t-flex"> <h1>sugar</h1><span><p>(${nf_sugars}g)</p></span>  </div>
-    <span><p>${parseInt((nf_sugars / nf_total)*100)}%</p></span>
+    <span><p>${parseInt((nf_sugars / nf_total) * 100)}%</p></span>
 </div> 
 <div class="total-fat">
     <div class="t-flex"> <h1>Iron</h1><span><p>(nf_iron)</p></span>  </div>
@@ -182,11 +181,11 @@ function displayCategoryItem(id) {
 </div> 
 <div class="total-fat">
     <div class="t-flex"> <h1>phosporus</h1><span><p>(${nf_p}g)</p></span>  </div>
-    <span><p>${parseInt((nf_p / nf_total)*100)}%</p></span>
+    <span><p>${parseInt((nf_p / nf_total) * 100)}%</p></span>
 </div> 
 <div class="t-p">
      <div class="t-flex"> <h1>potassium</h1><span><p>(${nf_potassium}g)</p></span>  </div>
-    <span><p>${parseInt((nf_potassium/nf_total)*100)}%</p></span>
+    <span><p>${parseInt((nf_potassium / nf_total) * 100)}%</p></span>
 </div> 
 
 </section>
@@ -252,34 +251,37 @@ function displayCategoryItem(id) {
       </div>
 `;
 
-          menuItems.innerHTML = shownItem;
+        menuItems.innerHTML = shownItem;
 
-          const AddFood = document.querySelector(".food-to-dashboard");
-          AddFood.addEventListener("click", () => {
-            addToDashboard(data.food);
-          });
-        })
-        .catch((error) => {
-          console.log(error);
+        const AddFood = document.querySelector(".food-to-dashboard");
+        AddFood.addEventListener("click", () => {
+          addToDashboard(data.food);
         });
-    }
-  );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 }
 
 function addToDashboard(data) {
-  localStorage.setItem("food", JSON.stringify(data));
-  fetch("/api/v1/foods", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(data),
-  })
-    .then(() => {
-      console.log("add Successfully");
+  if (token) {
+    localStorage.setItem("food", JSON.stringify(data));
+    fetch("/api/v1/foods", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(data),
     })
-    .catch((error) => {
-      console.log(error);
-    });
+      .then(() => {
+        console.log("add Successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } else {
+    window.location.href = "login.html";
+  }
 }
